250240202a21c0b7898d8dedc4461a7d6d1abbfa
MikhailOkhrimenko <mikhail.okhrimenko@logicify.com>
2017-01-24 13:53:49 +0200

    added list planets (ngFor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { Planets } from './shared/planets.model';
+import { PlanetsService } from './shared/planets.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  planetsList: Planets[] = [];
+
+  constructor(
+    private _planetsService: PlanetsService) {}
+
+  ngOnInit(): void {
+    this._planetsService.getPlanets()
+      .then(planets => this.planetsList = planets);
+  }
+
 }
