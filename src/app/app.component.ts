a84d82b13d809195558e46ec231a0c19de65cf70
MikhailOkhrimenko <mikhail.okhrimenko@logicify.com>
2017-01-24 16:55:51 +0200

    changed less to sass, added custom theme for material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { PlanetsService } from './shared/planets.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
