766ad484eae6de57adcb49a28ac65042bc537c79
MikhailOkhrimenko <mikhail.okhrimenko@logicify.com>
2017-01-25 14:37:14 +0200

    refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import { Planets } from './common/planets.model';
-import { PlanetsService } from './common/planets.service'
+import {Component} from "@angular/core";
 
 @Component({
   moduleId: module.id,
@@ -11,20 +6,4 @@ import { PlanetsService } from './common/planets.service'
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent implements OnInit {
-  planetsList: Planets[] = [];
-  selectedPlanet: Planets;
-
-  constructor(private _planetsService: PlanetsService) {}
-
-  ngOnInit(): void {
-    this._planetsService.getPlanets()
-      .then(planets => this.planetsList = planets);
-    this.selectedPlanet = new Planets();
-  }
-
-  showPlanetInfo(selPlanet): void {
-    this.selectedPlanet = selPlanet;
-  }
-
-}
+export class AppComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,33 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+
+import {Planets} from "../common/planets.model";
+import {PlanetsService} from "../common/planets.service";
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  planetsList: Planets[] = [];
+  selectedPlanet: Planets;
+
+  constructor(private router: Router,
+              private planetsService: PlanetsService) {}
+
+  ngOnInit(): void {
+    this.planetsService.getPlanets()
+      .then(planets => this.planetsList = planets);
+    this.selectedPlanet = new Planets();
+  }
 
   logout(): void {
     this.router.navigate(['./login']);
     console.log("Logout");
+  }
+
+  showPlanetInfo(selPlanet): void {
+    this.selectedPlanet = selPlanet;
   }
 
 }
