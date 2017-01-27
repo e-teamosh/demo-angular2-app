66517476289f546d05d9f43d825ad122b85ced94
MikhailOkhrimenko <mikhail.okhrimenko@logicify.com>
2017-01-27 17:22:40 +0200

    cleanup code

--- src/app/common/interfaces/user-password.interface.ts
+++ src/app/common/interfaces/user-password.interface.ts
@@ -1,8 +1,5 @@
 export interface UserPassword {
   password: string;
-  confirmPassword?: string;
   setPassword(password: string): void;
   getPassword(): string;
-  setConfirmPassword?(confirmPassword: string): void;
-  getConfirmPassword?(): string;
 }
