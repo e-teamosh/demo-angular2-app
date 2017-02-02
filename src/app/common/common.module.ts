073c0a1f3d34bd9e43eba3f66f08238718ec657d
MikhailOkhrimenko <mikhail.okhrimenko@logicify.com>
2017-02-02 11:04:12 +0200

    cleanup code (remove planets)

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from "@angular/core";
 import {AuthGuardService} from "./services/auth-guard.service";
 import {CanDeactivateGuardService} from "./services/can-deactivate-guard.service";
 import {CityService} from "./services/city.service";
-import {PlanetsService} from "./services/planets.service";
 import {UsersService} from "./services/users.service";
 
 @NgModule({
@@ -13,7 +12,6 @@ import {UsersService} from "./services/users.service";
     AuthGuardService,
     CanDeactivateGuardService,
     CityService,
-    PlanetsService,
     UsersService
   ],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from "@angular/core";
-import {Planet} from "../common/models/planet.model";
 import {CityService} from "../common/services/city.service";
-import {PlanetsService} from "../common/services/planets.service";
 import {WFHttpService} from "../core/http-client/services/http.service";
 
 @Component({
@@ -11,11 +9,8 @@ import {WFHttpService} from "../core/http-client/services/http.service";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  planetsList: Planet[] = [];
-  selectedPlanet: Planet;
 
-  constructor(private planetsService: PlanetsService,
-              private http: WFHttpService,
+  constructor(private http: WFHttpService,
               private cityService: CityService) {
   }
 
@@ -27,21 +22,12 @@ export class HomeComponent implements OnInit {
       })
       .then(result => {
         console.dir(result);
-        return this.cityService.getCityListByQuery('new Y', 'US');
+        return this.cityService.getCityListByQuery('Bos', 'US');
       })
       .then(result => console.dir(result));
-
-    this.planetsService.getPlanets()
-      .then(planets => this.planetsList = planets);
-    this.selectedPlanet = new Planet();
 
     this.http.get('weather?q=London')
       .then(res => console.log(res))
       .catch(error => console.log(error));
   }
-
-  showPlanetInfo(selPlanet: Planet): void {
-    this.selectedPlanet = selPlanet;
-  }
-
 }
